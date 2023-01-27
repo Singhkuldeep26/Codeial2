67c3c540726b28110e128fea4c6a20deefebc32b
kuldeep singh <pssingh1145@gmail.com>
2023-01-27 19:40:47 +0530

    final3

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose');
 const env=require('./environment');
 mongoose.set("strictQuery", false);
-//mongoose.connect(`mongodb://localhost/${env.db}`);
+mongoose.connect(`mongodb://127.0.0.1:27017/codeial_development`);
 //mongodb+srv://kuldeepsingh:kuldeep11@cluster0.d0tsd82.mongodb.net/test
-mongoose.connect("mongodb+srv://kuldeepsingh:Kuldeep11@cluster0.d0tsd82.mongodb.net/?retryWrites=true&w=majority");
+//mongoose.connect("mongodb+srv://kuldeepsingh:Kuldeep11@cluster0.d0tsd82.mongodb.net/?retryWrites=true&w=majority");
 const db=mongoose.connection;
 
 db.on('error',console.error.bind(console,"Error connecting to MongoDB"));
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const logger=require('morgan');
 const cookieParser=require('cookie-parser');
 const app=express();
 require('./config/view-helpers')(app);
-const port=8000;
+const port=process.env.PORT ||8000;
 
 const expressLayouts=require('express-ejs-layouts');
 
