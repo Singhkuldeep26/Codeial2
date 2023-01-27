9d0c7a0c0792aab166a9e02112a3bcb28ce2abe3
kuldeep singh <pssingh1145@gmail.com>
2023-01-27 17:25:50 +0530

    T

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ if(env.name=='development'){
     }));
 }
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended:false}));
 app.use(cookieParser());
 
 app.use(express.static(env.asset_path));
